878a3f0524ba6fc6172bbd18e1444c784d02e74f
PedroOrellana98 <porellanaj@est.ups.edu.ec>
2020-05-23 17:39:35 -0500

    Commit estructura login

--- php/public/controladores/login.php
+++ php/public/controladores/login.php
@@ -6,33 +6,15 @@
     $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
     $contrasena = isset($_POST["contraseña"]) ? trim($_POST["contraseña"]) : null;
     
-    $sql = "SELECT usu_correo, usu_password FROM usuario WHERE usu_correo = '$usuario' and usu_password =  md5('$contrasena')";
+    //$sql = "SELECT usu_correo, usu_password FROM usuario WHERE usu_correo = '$usuario' and usu_password =  md5('$contrasena')";
+    $sql = "SELECT `correo`, `contrasena` FROM `usuario` WHERE correo = '$usuario' and contrasena =  md5('$contrasena')";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($fila = $result->fetch_assoc()) {
             $_SESSION['isLogged'] = TRUE;
-            //printf ("%s (%s)\n", $fila["usu_correo"], $fila["usu_password"]);
             header("Location: ../../admin/vista/usuario/index.php");
         }
-    } else {  
-        echo "Mal ingresado";
+    } else {
         header("Location: ../vista/login.html");
     }
-
-    //$sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena')";
-    //$sql1 = "SELECT usu_correo, usu_password FROM usuario";
-
-    //$result = $conn->query($sql);       
-    //$result1 = $conn->query($sql1);
-
-    /*$sql = "SELECT usu_correo, usu_password FROM usuario";
-    $result = $conn->query($sql);
-
-    if (!$result) {
-        trigger_error('Invalid query: ' . $conn->error);
-    }else {
-        while ($fila = $result->fetch_assoc()) {
-            printf ("%s (%s)\n", $fila["usu_correo"], $fila["usu_password"]);
-        }
-    }*/
 ?>
