8dc8408bf346b935e75f342d484a35442b49e7ec
PedroOrellana98 <porellanaj@est.ups.edu.ec>
2020-05-30 16:35:34 -0500

    Commit estructura de seguridad

--- php/public/controladores/login.php
+++ php/public/controladores/login.php
@@ -6,12 +6,17 @@
     $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
     $contrasena = isset($_POST["contraseña"]) ? trim($_POST["contraseña"]) : null;
 
-    $sql = "SELECT `correo`, `contrasena` FROM `usuario` WHERE correo = '$usuario' and contrasena =  md5('$contrasena')";
+    $sql = "SELECT `rol` FROM `usuario` WHERE correo = '$usuario' and contrasena =  md5('$contrasena')";
+
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($fila = $result->fetch_assoc()) {
             $_SESSION['isLogged'] = TRUE;
-            header("Location: ../../admin/vista/usuario/index.php?correo=$usuario");
+            if($fila['rol'] == "u"){
+                echo("usuario");
+            }else if($fila['rol'] == "a"){
+                echo("admin");
+            }
         }
     } else {
         header("Location: ../vista/login.html");
